11128f9f7baaabfdf91774835e9711a9735e5b83
Nooraniamin <133563755+Nooraniamin@users.noreply.github.com>
2026-03-20 13:12:12 +0500

    Add Zod schemas and inferred types

---
+++ src/lib/zod.tsx
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export const FacultySchema = z.object({
+  firstname: z.string().min(2).max(50),
+  lastname: z.string().min(2).max(50).optional(),
+  email: z.string(),
+  module: z.string(),
+  status: z.boolean().default(false),
+});
+export const StudentSchema = z.object({
+    firstname: z.string().min(2).max(50),
+    lastname: z.string().min(2).max(50).optional(),
+    email: z.string(),
+    Batch: z.coerce.number().int().positive(),
+    status: z.boolean().default(false),
+
+});
+export const SessionSchema = z.object({
+    title: z.string().min(2).max(50),
+    Description: z.string().min(2).max(50).optional(),
+    Module: z.string(),
+    faculty: z.string(),
+    status: z.boolean().default(false),
+
+});
+export const ModuleSchema = z.object({
+    title: z.string().min(2).max(50),
+    Description: z.string().min(2).max(50).optional(),
+    Year: z.string(),
+});
+export const ToastSchema = z.object({
+    messge: z.string(),
+})
+export type IFaculty = z.infer<typeof FacultySchema>;
+export type IStudent = z.infer<typeof StudentSchema>;
+export type ISession = z.infer<typeof SessionSchema>;
+export type IModule = z.infer<typeof ModuleSchema>;
+export type IToast = z.infer<typeof ToastSchema>;
