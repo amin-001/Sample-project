1e8a784694ec4fc81ae2f0bf687c8160fb9234e0
Nooraniamin <133563755+Nooraniamin@users.noreply.github.com>
2026-03-20 13:13:59 +0500

    Main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import { Button } from "@base-ui/react/button"
-
+import { useEffect } from "react";
+import { toast, Toaster } from "sonner";
+import { Button } from "./components/ui/button";
 
 function App() {
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      toast.success("This is the toast that will be shown.");
+    }, 0);
+    return () => clearTimeout(timerId);
+  }, []);
+
   return (
     <div>
-      <div className="text-5xl">amin</div>
+      <Button
+      variant="outline"
+      onClick={() => {
+        console.log("clicked!");
 
-      <Button type="button" variant={"outline"}>
-        Click me
-      </Button>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        });
+      }}
+    >
+      Show Toast
+    </Button>
+     <Toaster />
+      <AddFacultyDialog>
+        <AddFacultyForm />
+      </AddFacultyDialog>
+      <AddStudentDialog>
+        <AddStudentForm />
+      </AddStudentDialog>
+      <AddSessionDialog>
+        <AddSessionForm />
+      </AddSessionDialog>
+      <AddModuleDialog>
+        <AddModuleForm />
+      </AddModuleDialog>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
