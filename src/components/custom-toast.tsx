589d0ec08fe1aaae6219831a3466d3dbf9b2c82d
Nooraniamin <133563755+Nooraniamin@users.noreply.github.com>
2026-03-24 16:23:02 +0500

    Work of navigation bar, student, student tables, theme work

---
+++ src/components/custom-toast.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export function SonnerDemo() {
+  return (
+    <Button
+      variant="outline"
+      onClick={() => {
+        console.log("clicked!");
+        toast.success("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        });
+      }}
+    >
+      Show Toast
+    </Button>
+  );
+}
